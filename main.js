002ad837900ec321fc5ea1f37a748d7a1a3a6656
USER <user@USERnoMacBook-ea.local>
2020-04-19 15:31:50 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const person = {
+    name: ['Code', 'Mafia'],
+    age: 32,
+    gender: 'male',
+    interests: {
+        sports: 'soccer',
+        music: 'piano'
+    }
+};
+
+console.log(person.name[1]);
+// console.log(person.interests.music);
