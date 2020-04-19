6c3827885095dbd71a4bc64006bf16571e1e2c8f
USER <user@USERnoMacBook-ea.local>
2020-04-19 15:35:41 +0900

    second commit

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ const person = {
     interests: {
         sports: 'soccer',
         music: 'piano'
+    },
+    getFullName: function() {
+        console.log(this.name[0] + this.name[1])
     }
 };
-
-console.log(person.name[1]);
+const agekey = 'age';
+person[agekey] = 12;
+console.log(person.age);
+person.getFullName();
+// person.age = 12;
+// console.log(person.name[1]);
 // console.log(person.interests.music);
